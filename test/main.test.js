d1ff021c0bad69cd98216847853752e4d4e49c0d
kolljohnson <kjohnson@pillartechnology.com>
2017-07-25 11:32:36 -0500

    Initial Commit

---
+++ test/main.test.js
@@ -0,0 +1,9 @@
+//@flow
+import { expect } from 'chai';
+import Main from '../src/main';
+
+describe('Main', () => {
+    it('should be a class', () => {
+	expect(new Main()).to.not.equal(undefined);
+    });
+});
